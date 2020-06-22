e613527fedb1d2ee26739501e96ee974d85c9293
Jaboa <jaboahowell@gmail.com>
2020-06-22 15:49:02 -0600

    finished

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,4 +1,18 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {}
+function objectProperties(someObj) {
+  for (key in someObj) {
+    console.log(`${key}: ${someObj[key]}`);
+  }
+}
+
+function student(studentId, studentName, studentGrade) {
+  this.studentId = studentId;
+  this.studentName = studentName;
+  this.studentGrade = studentGrade;
+}
+
+let howell = new student("1234", "jaboa howell", "12");
+
+objectProperties(howell);
